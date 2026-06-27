c60f8dd61a6d6f44dc7761ff5c2dd29185cb563d
David Herzfeld <dherzfeld@tveyes.com>
2026-06-27 19:45:29 -0400

    web: drop VITE_PASSENGER_API_V2 type declaration (T12 cleanup)

--- web/src/vite-env.d.ts
+++ web/src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_PASSENGER_API_V2?: string;
   readonly VITE_CANVAS_API?: string;
   readonly VITE_BUILD_SHA?: string;
 }
